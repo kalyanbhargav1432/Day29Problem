e4f060ec0f11bb62840fe187342aa95564d6ee3a
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-13 10:20:48 +0530

    Added:Functionality to add data into csvfile

---
+++ indian_census_analyser/CSVStateClass.java
@@ -0,0 +1,27 @@
+package indian_census_analyser;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVStateClass {
+	public static void main(String[] args) {
+		String path = "E:\\CSVFile\\IndiaStateCensusData - IndiaStateCensusData.csv";
+		String line = "";
+		try {
+
+			BufferedReader bufferReader = new BufferedReader(new FileReader(path));
+			while ((line = bufferReader.readLine()) != null) {
+				String[] values = line.split(",");
+				System.out.println("State:" + values[0] + ",Population:" + values[1] + ",AreaInSqKm:" + values[2]
+						+ ",DensityPerSqKm:" + values[3]);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
