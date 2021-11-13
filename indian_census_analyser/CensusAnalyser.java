98211a8d9a577c0517bad52c1da5b6621db8223d
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-13 22:17:24 +0530

    Refactored:Functionality to add data into csv file

--- indian_census_analyser/CensusAnalyser.java
+++ indian_census_analyser/CensusAnalyser.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class CSVStateClass {
+public class CensusAnalyser {
 	public static void main(String[] args) {
 		String path = "E:\\CSVFile\\IndiaStateCensusData - IndiaStateCensusData.csv";
 		String line = "";
